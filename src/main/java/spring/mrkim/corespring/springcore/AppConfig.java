aedd3f47b267652197bf93421129f7f08c81456d
ChanYoungHong <cksdud9883@naver.com>
2022-07-18 23:55:18 +0900

    Bean 주입과 Test

--- src/main/java/spring/mrkim/corespring/springcore/AppConfig.java
+++ src/main/java/spring/mrkim/corespring/springcore/AppConfig.java
@@ -1,22 +1,39 @@
 package spring.mrkim.corespring.springcore;
 
-import spring.mrkim.corespring.springcore.discount.FixDiscountPolicy;
-import spring.mrkim.corespring.springcore.order.Order;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.mrkim.corespring.springcore.discount.DiscountPolicy;
+import spring.mrkim.corespring.springcore.discount.RateDiscountPolicy;
 import spring.mrkim.corespring.springcore.order.OrderService;
 import spring.mrkim.corespring.springcore.order.OrderServiceImpl;
+import spring.mrkim.corespring.springcore.repository.MemberRepository;
 import spring.mrkim.corespring.springcore.repository.MemoryMemberRepository;
 import spring.mrkim.corespring.springcore.service.MemberService;
 import spring.mrkim.corespring.springcore.service.MemberServiceImpl;
 
 // 객체의 생성과 연결은 AppConfig가 담당한다.
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
